69a88167494e1ebec939a076067922807c7d6c1b
GandresCoello18 <goyeselecoca@gmail.com>
2020-06-15 00:36:13 -0500

    restaurar domain production

--- src/util/verifi-local-token.js
+++ src/util/verifi-local-token.js
@@ -1,5 +1,3 @@
-import axios from "axios";
-
 export function exist_token(cookie_token) {
   // console.log(cookie_token);
   if (cookie_token == undefined || cookie_token == "undefined") {
@@ -7,21 +5,9 @@ export function exist_token(cookie_token) {
   } else {
     return true;
   }
-
-  /*axios({
-    method: "post",
-    url: `${domain()}/api/login/vida-token`,
-    headers: { "access-token": cookie_token },
-  }).then((info) => {
-    if (info.data.feeback == "Token activo") {
-      return true;
-    } else {
-      return false;
-    }
-  });*/
 }
 
 export function domain() {
-  //return "https://api-laboratorio-juanito.herokuapp.com";
-  return "http://127.0.0.1:7000";
+  return "https://api-laboratorio-juanito.herokuapp.com";
+  //return "http://127.0.0.1:7000";
 }
